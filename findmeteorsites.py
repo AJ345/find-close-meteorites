17a630aaf30646b1f5b0fd1f0482979a7bcc1c13
Anoushka Jawale <anoushkajawale@gmail.com>
2020-06-11 11:10:17 -0400

    Don't run script code when imported

--- findmeteorsites.py
+++ findmeteorsites.py
@@ -16,19 +16,20 @@ def calc_dist(lat1, lon1, lat2, lon2):
 
     return 6372.8 * 2 * math.asin(math.sqrt(h))
 
-print("Loading...")
-user_lat = input("What is your latitude?")
-user_long = input("What is your longitude?")
+if __name__ == "__main__" : 
+	print("Loading...")
+	user_lat = input("What is your latitude?")
+	user_long = input("What is your longitude?")
 
-distances = []
+	distances = []
 
 
-for meteor in meteor_data: 
-  if 'reclat' and 'reclong' in meteor : 
-    meteor['distance'] = calc_dist(float(user_lat), float(user_long), float(meteor['reclat']), float(meteor['reclong']))
-    distances.append(meteor['distance'])
-    if meteor['distance'] < 1000 : 
-      print(meteor['name'], meteor['year'], meteor['distance'])
+	for meteor in meteor_data: 
+  		if 'reclat' and 'reclong' in meteor : 
+    			meteor['distance'] = calc_dist(float(user_lat), float(user_long), float(meteor['reclat']), float(meteor['reclong']))
+    			distances.append(meteor['distance'])
+    			if meteor['distance'] < 1000 : 
+     				print(meteor['name'], meteor['year'], meteor['distance'])
 
 
   
